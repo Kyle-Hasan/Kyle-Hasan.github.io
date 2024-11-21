11166b3c72b2bd8867d6c673071a5f4b6416e56b
Kyle Hasan <kylehasan1@gmail.com>
2024-11-20 18:39:38 -0700

    Fix about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => (
       <h1 className="text-4xl font-extrabold text-gray-800 mb-6">
         About Me
       </h1>
-      <p className="text-lg text-gray-600 mb-6">I am a fifth-year software engineering student at the University of Calgary with a passion for programming. I specialize in web development and am currently expanding my knowledge of cloud technologies. I will be graduating in December 2024 and am seeking a new graduate software developer position. On this site, you can find more information about me and my work.
+      <p className="text-lg text-gray-600 mb-6">I am a fifth-year software engineering student at the University of Calgary with a passion for software development. I specialize in web development and am currently expanding my knowledge of cloud technologies. I will be graduating in December 2024 and am seeking a new graduate software developer position. On this site, you can find more information about me and my work.
       </p>
     </div>
   </section>
