c89c687b24c1acd2cb7d26d14385cc7bb80dcd85
Kyle Hasan <kylehasan1@gmail.com>
2024-12-08 02:30:57 -0700

    fix styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => (
   <section id="about" className="bg-white py-10">
-  <div className="container mx-auto px-6 text-center">
+  <div className="container mx-auto px-6 text-center flex flex-col justify-center items-center">
  
     <div className="mb-8">
       <h1 className="text-4xl font-extrabold text-gray-800">
@@ -20,7 +20,7 @@ const About = () => (
     <h2 className="text-2xl font-bold text-gray-800 mb-6">
       About Me
     </h2>
-    <p className="text-lg text-gray-600 mb-6">
+    <p className="text-lg text-gray-600 mb-6 xl:w-[50%] lg:w-2/3 text-left">
       I am a fifth-year software engineering student at the University of Calgary passionate about software development. I specialize in web development and am currently expanding my knowledge of cloud technologies. I will be graduating in December 2024 and am seeking a new graduate software developer position. On this site, you can find more information about me and my work.
     </p>
   </div>
