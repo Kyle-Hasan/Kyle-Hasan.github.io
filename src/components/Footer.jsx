086680fdd50643a2193b4974726999da765d5d59
Kyle Hasan <kylehasan1@gmail.com>
2024-12-07 22:04:35 -0700

    Fix wording and mobile navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-gray-800 text-white py-4">
+    <footer className="bg-gray-800 text-white py-5">
       <div className="container mx-auto text-center">
         <p>© {currentYear} Kyle Hasan. All rights reserved.</p>
       </div>
